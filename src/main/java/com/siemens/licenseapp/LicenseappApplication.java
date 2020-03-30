8f849289672a4800fdbf71cd0b9f21eda1e753f7
Simion Vlad <simivlad23@yahoo.ro>
2020-03-30 12:10:37 +0300

    Initial commit

---
+++ src/main/java/com/siemens/licenseapp/LicenseappApplication.java
@@ -0,0 +1,13 @@
+package com.siemens.licenseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siemens/licenseapp/LicenseappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siemens.licenseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
